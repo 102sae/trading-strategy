7941e3054bfbd6f39a4f2072e809a3ee88156286
102sae <102chimee@gmail.com>
2023-11-15 17:12:26 +0900

    add : 매도 부분 추가

--- jeongmin.py
+++ jeongmin.py
@@ -7,12 +7,15 @@ from PyQt5.QtWidgets import *
 import pandas as pd
 import GiExpertControl as giLogin
 import GiExpertControl as giTop50Show
+import GiExpertControl as giJongmokRealTime
 from pythonUI import Ui_MainWindow
 import time
 import datetime
 
+RTOCX1 = giJongmokRealTime.NewGiExpertModule()
+
 main_ui = Ui_MainWindow()
-top10 = []
+
 class indiWindow(QMainWindow):
 
     def get_date(self):
@@ -34,7 +37,11 @@ class indiWindow(QMainWindow):
 
         main_ui.pushButton.clicked.connect(self.buy_stock)
         main_ui.pushButton_2.clicked.connect(self.chegyeol_show)
+        main_ui.pushButton_3.clicked.connect(self.sell_stock)
+        main_ui.pushButton_4.clicked.connect(self.show_silsigan_jango)
+
         giTop50Show.SetCallBack('ReceiveData', self.giTop50Show_ReceiveData)
+        RTOCX1.SetCallBack('ReceiveRTData', self.RTOCX1_ReceiveRTData)
         
         print(giLogin.GetCommState())
         if giLogin.GetCommState() == 0: # 정상
@@ -63,7 +70,7 @@ class indiWindow(QMainWindow):
         PW_text = main_ui.textEdit_2.toPlainText()
         jongmok_code = main_ui.textEdit_3.toPlainText()
         jumun_suryang = main_ui.textEdit_4.toPlainText()
-        
+
         print(gaejwa_text,PW_text,jongmok_code,jumun_suryang)
         TR_Name = "SABA101U1"          
         ret= giTop50Show.SetQueryName(TR_Name)  
@@ -99,8 +106,6 @@ class indiWindow(QMainWindow):
             print(f"Error in RequestData: {e}")
             rqid = None  # 또는 다른 초기값으로 설정
     
-
-
         print(type(rqid))
         print('Request Data rqid: ' + str(rqid))
         self.rqidD[rqid] = TR_Name
@@ -130,6 +135,72 @@ class indiWindow(QMainWindow):
         except Exception as e:
             print(f"Error in RequestData: {e}")
             rqid = None
+
+    #실시간 잔고 받아오기
+    def show_silsigan_jango(self):
+        TR_Name = "AD"             
+        print("===================실시간=====================")
+        rqid = RTOCX1.RequestRTReg("AD","*")
+        print(type(rqid))
+        print('Request Data rqid: ' + str(rqid))        
+
+    #자동 매도 신청
+    def sell_stock(self):
+        gaejwa_text = main_ui.textEdit.toPlainText()
+        PW_text = main_ui.textEdit_2.toPlainText()
+        jongmok_code = main_ui.textEdit_3.toPlainText()
+        jumun_suryang = main_ui.textEdit_4.toPlainText()
+
+        print(gaejwa_text,PW_text,jongmok_code,jumun_suryang)
+        TR_Name = "SABA101U1"      
+        ret= giTop50Show.SetQueryName(TR_Name)  
+        ret = giTop50Show.SetSingleData(1,"01")#계좌 상품(항상01)
+        ret = giTop50Show.SetSingleData(2,PW_text)#계좌 비밀번호
+        ret = giTop50Show.SetSingleData(0,gaejwa_text)#계좌번호
+        ret = giTop50Show.SetSingleData(3,"")#계좌 관리부점코드
+        ret = giTop50Show.SetSingleData(4,"")#시장거래구분
+        ret = giTop50Show.SetSingleData(5,"0")#선물대용매도여부
+        ret = giTop50Show.SetSingleData(6,"00")#신용거래구분
+        ret = giTop50Show.SetSingleData(7,"1") #매도/매수 구분
+        ret = giTop50Show.SetSingleData(8,jongmok_code)#종목코드
+        ret = giTop50Show.SetSingleData(9,jumun_suryang)#주문수량
+        ret = giTop50Show.SetSingleData(10,"")#주문가격
+        ret = giTop50Show.SetSingleData(11,"1")#정규시간외구분코드
+        ret = giTop50Show.SetSingleData(12,"1")#시장가
+        ret = giTop50Show.SetSingleData(13,"0")#주문조건코드
+        ret = giTop50Show.SetSingleData(14,"0")#신용대출통합주문구분코드
+        ret = giTop50Show.SetSingleData(15,"")#신용대출일자
+        ret = giTop50Show.SetSingleData(16,"")#원주문번호
+        ret = giTop50Show.SetSingleData(17,"") 
+        ret = giTop50Show.SetSingleData(18,"")
+        ret = giTop50Show.SetSingleData(19,"")
+        ret = giTop50Show.SetSingleData(20,"")#프로그램매매여부
+        ret = giTop50Show.SetSingleData(21,"Y")#결과메세지처리여부
+        
+        try:
+            rqid = giTop50Show.RequestData()
+            print(type(rqid))
+            print('Request Data rqid: ' + str(rqid))
+            self.rqidD[rqid] = TR_Name
+        except Exception as e:
+            print(f"Error in RequestData: {e}")
+            rqid = None  # 또는 다른 초기값으로 설정
+    
+        print(type(rqid))
+        print('Request Data rqid: ' + str(rqid))
+        self.rqidD[rqid] = TR_Name
+
+    def RTOCX1_ReceiveRTData(self,giCtrl,RealType):
+        if RealType == "AD":
+            main_ui.tableWidget_3.insertRow(main_ui.tableWidget_2.rowCount())
+            final_rowCount = main_ui.tableWidget_3.rowCount() - 1
+            main_ui.tableWidget_3.setItem(final_rowCount,0,QTableWidgetItem(str(giCtrl.GetSingleData(2))))#종목코드
+            main_ui.tableWidget_3.setItem(final_rowCount,1,QTableWidgetItem(str(giCtrl.GetSingleData(3))))#종목명
+            main_ui.tableWidget_3.setItem(final_rowCount,2,QTableWidgetItem(str(giCtrl.GetSingleData(6))))#잔고수량
+            main_ui.tableWidget_3.setItem(final_rowCount,3,QTableWidgetItem(str(giCtrl.GetSingleData(7))))#평균단가
+            main_ui.tableWidget_3.setItem(final_rowCount,2,QTableWidgetItem(str(giCtrl.GetSingleData(17))))#현재가
+            main_ui.tableWidget_3.setItem(final_rowCount,3,QTableWidgetItem(str(giCtrl.GetSingleData(13))))#손익률
+
     
 
 
@@ -139,9 +210,6 @@ class indiWindow(QMainWindow):
 
         TR_Name = self.rqidD[rqid]
         tr_data_output = []
-
-        for i in range(0,10):
-            top10.append({"code": str(giCtrl.GetMultiData(i, 0)), "price" : str(giCtrl.GetMultiData(i, 2))})
         
         print("TR_name : ",TR_Name)
 
